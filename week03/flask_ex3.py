e9ed672a03a1f8da68bfa40b20e842052f2d1264
danuni <hekdms@jbnu.ac.kr>
2023-09-20 08:46:28 +0900

    week03

--- week03/flask_ex3.py
+++ week03/flask_ex3.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def first_page():
-    return "<a href='/'>첫페이지</a> <a href='/hello'>hello</a><p>첫페이지 입니다.</p>"
+    return """
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET" action="/gugu">
+        <h2>구구단 출력하기</h2>
+        <label>단 :
+            <input type="text" name="dan">
+        </label>
+        <button type="submit">출력</button>
+    </form>
+</body>
+</html>
+"""
 
+# 지난주 /gugu/7
 
-@app.route("/gugu/<dan>")
-def gugudan(dan):
-    dan = int(dan)
+# 이번주 /gugu?dan=7
+
+@app.route("/gugu")
+def gugudan():
+    dan = int(request.args.get('dan'))
     resp = ""
     resp += "<html>\n"
     resp += '<meta charset="utf-8">\n'
@@ -25,11 +48,5 @@ def gugudan(dan):
 
 
     return resp
-@app.route("/hello")
-def hello_world():
-
-
-    return "<a href='/'>첫페이지</a> <a href='/hello'>hello</a><p>Hello, World!</p>"
-
 
 app.run(host="0.0.0.0")
