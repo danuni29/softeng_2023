f83b9360c944e2aaadf7ec72102760f6336f6562
danuni <hekdms@jbnu.ac.kr>
2023-09-27 01:35:56 +0900

    홈페이지 만들기

--- docs/add_two_numbers.js
+++ docs/add_two_numbers.js
@@ -1,7 +1,7 @@
 function doSomething() {
     let a = document.getElementById('inputA').value;
-    let b = document.getElementById('inputB').value;
+    // let b = document.getElementById('inputB').value;
     document.getElementById("valueA").innerHTML = a;
-    document.getElementById("valueB").innerHTML = b;
-    document.getElementById("valueC").innerHTML = Number(a) + Number(b);
+    // document.getElementById("valueB").innerHTML = b;
+    // document.getElementById("valueC").innerHTML = Number(a) + Number(b);
 }
